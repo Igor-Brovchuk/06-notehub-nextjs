7ffe377d0d859a89da7c423eae3156721f641ea3
Igor Brovchuk <i.brovchuk@gmail.com>
2026-05-28 21:44:23 +0300

    update

---
+++ app/notes/Notes.client.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { fetchNotes } from '@/lib/api'
+import NoteList from '@/components/NoteList/NoteList'
+import Pagination from '@/components/Pagination/Pagination'
+import css from '@/app/notes/Notes.module.css'
+import { useState } from 'react'
+import Modal from '@/components/Modal/Modal'
+import NoteForm from '@/components/NoteForm/NoteForm'
+import { useDebouncedCallback } from 'use-debounce'
+import SearchBox from '@/components/SearchBox/SearchBox'
+
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [page, setPage] = useState(1)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ['notes', query, page],
+    queryFn: () => fetchNotes({ query, page }),
+    placeholderData: keepPreviousData,
+  })
+
+  const updateQuery = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(e.target.value)
+      setPage(1)
+    },
+    1000,
+  )
+
+  const notes = data?.notes || []
+  const totalPages = data?.totalPages ?? 1
+
+  const handleOpenModal = () => setIsModalOpen(true)
+  const handleCloseModal = () => setIsModalOpen(false)
+
+  return (
+    <>
+      <div className={css.app}>
+        <header className={css.toolbar}>
+          <SearchBox onSearch={updateQuery} />
+          {totalPages > 1 && (
+            <Pagination
+              totalPages={totalPages}
+              page={page}
+              onPageChange={setPage}
+            />
+          )}
+          <button className={css.button} onClick={handleOpenModal}>
+            Create note +
+          </button>
+        </header>
+
+        <main>
+          {isLoading && <p>Loading notes...</p>}
+
+          {(isError || (isSuccess && notes.length === 0)) && (
+            <p>Failed to load notes</p>
+          )}
+
+          {isSuccess && notes.length > 0 && <NoteList notes={notes} />}
+        </main>
+      </div>
+
+      {isModalOpen && (
+        <Modal onClose={handleCloseModal}>
+          <NoteForm onClose={handleCloseModal} />
+        </Modal>
+      )}
+    </>
+  )
+}
---
+++ lib/api.ts
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import type { Note, NoteTag } from '../type/note'
+
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN
+if (!token) {
+  throw new Error('Missing NEXT_PUBLIC_NOTEHUB_TOKEN')
+}
+
+const notehubApi = axios.create({
+  baseURL: 'https://notehub-public.goit.study/api',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+
+interface FetchNotesResponse {
+  notes: Note[]
+  totalPages: number
+}
+
+interface FetchNotesParams {
+  page?: number
+  perPage?: number
+  query?: string
+}
+
+export interface CreateNoteParams {
+  title: string
+  content: string
+  tag: NoteTag
+}
+
+export const fetchNotes = async ({
+  page = 1,
+  perPage = 12,
+  query = '',
+}: FetchNotesParams): Promise<FetchNotesResponse> => {
+  const response = await notehubApi.get<FetchNotesResponse>('/notes', {
+    params: {
+      page,
+      perPage,
+      search: query.trim() || undefined,
+    },
+  })
+  return response.data
+}
+
+export const createNote = async (newNote: CreateNoteParams): Promise<Note> => {
+  const response = await notehubApi.post<Note>('/notes', newNote)
+
+  return response.data
+}
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const response = await notehubApi.delete<Note>(`/notes/${id}`)
+
+  return response.data
+}
+
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const { data } = await notehubApi.get<Note>(`/notes/${id}`)
+  return data
+}
